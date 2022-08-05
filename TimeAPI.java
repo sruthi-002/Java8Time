2da3f0b1d0798b91af8f82e4211cda42c8e2b4f3
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-05 17:00:13 +0530

    Add files via upload

---
+++ TimeAPI.java
@@ -0,0 +1,12 @@
+﻿package com.company;
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        LocalDate yesterday = date.minusDays(1);
+        LocalDate Tommorow = yesterday.plusDays(2);
+        System.out.println("Today is "+date);
+        System.out.println("Yesterday is "+yesterday);
+        System.out.println("Tommorow is "+Tommorow);
+    }
+}
